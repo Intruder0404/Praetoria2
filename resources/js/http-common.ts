48e5a7c3697911b9d643e3de1479a5eeb06fc4f1
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-02-19 14:09:01 -0500

    fix router

--- resources/js/http-common.ts
+++ resources/js/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: '/api/'+import.meta.env.APP_API_URL,
+  baseURL: import.meta.env.APP_API_URL,
   headers: {
     'Authorization': `Bearer ${localStorage.getItem("access_token")}`
   }
