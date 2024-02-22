97b88979f6f1779042a0a45747cd2a144720e5e8
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-02-21 20:41:04 -0500

    fix router

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <link rel="icon" href="public/logo/praetoria-1-transparent.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Praetoria</title>
