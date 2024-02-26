3ef1012d96806654bbf5e8b808e58527bf018fac
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-02-25 23:21:59 -0500

    database modifications

--- database/migrations/2024_02_26_032944_create_orders_table.php
+++ database/migrations/2024_02_26_032944_create_orders_table.php
@@ -14,11 +14,20 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('order_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class);
             $table->foreignIdFor(OrderStatus::class);
             $table->timestamps();
+        });
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->foreignIdFor(Order::class);
+            $table->foreignIdFor(Product::class);
         });
     }
 
