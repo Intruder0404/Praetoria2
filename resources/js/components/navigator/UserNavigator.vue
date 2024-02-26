3b12d0127d7678051dde57e0be5493e04f498135
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-02-26 12:32:33 -0500

    fix big stuff

--- resources/js/components/navigator/UserNavigator.vue
+++ resources/js/components/navigator/UserNavigator.vue
@@ -1,5 +1,6 @@
 <template>
     <v-navigation-drawer
+        permanent
     >
        <v-list>
             <v-list-item
@@ -14,11 +15,11 @@
         <v-list density="compact" nav>
             <v-list-item prepend-icon="mdi-account" title="User Account"
                          to="/account"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-child-outline" title="Personnage" value="personnage"
+            <v-list-item prepend-icon="mdi-account-child-outline" title="Personnages(under construction)" value="personnage"
                          to="/character"></v-list-item>
-            <v-list-item prepend-icon="mdi-human-male-female-child" title="Family" value="family"
+            <v-list-item prepend-icon="mdi-human-male-female-child" title="Family(under construction)" value="family"
                          to="/family"></v-list-item>
-            <v-list-item prepend-icon="mdi-home-group" title="Reservation" value="reservation"
+            <v-list-item prepend-icon="mdi-home-group" title="Reservation(under construction)" value="reservation"
                          to="/reservation"></v-list-item>
             <v-list-group open-on-hover v-if="user.type.id === 2" prepend-icon="mdi-account-group"
                           title="Administration" value="admin">
