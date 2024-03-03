88875c64a27c42b9c57fa5d0c6a1f8c28fc9e85b
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-03-03 18:50:05 -0500

    fix big stuff

--- resources/js/components/forms/admin/GuildForm.vue
+++ resources/js/components/forms/admin/GuildForm.vue
@@ -2,22 +2,14 @@
     <v-form ref="guildForm" v-model="valid" lazy-validation>
         <v-card class="px-4">
             <v-card-title>
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                {{ modifiedFamily.name }} Family Management
-=======
+
                 {{ guild.name }} Family Management
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
             </v-card-title>
             <v-card-text>
                 <v-row>
                     <v-col cols="12" sm="6" md="6">
                         <v-text-field
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                            v-if="user.characters.find(c => c.isActive).family.pater_familia.id === user.id"
-                            v-model="modifiedFamily.name"
-=======
                             v-model="guild.name"
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                             :rules="[rules.required]"
                             label="Name"
                             required
@@ -28,27 +20,15 @@
                     </v-col>
                     <v-col cols="12" sm="6" md="6">
                         <v-checkbox
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                            v-if="user.characters.find(c => c.isActive).family.pater_familia.id === user.id"
-                            v-model="modifiedFamily.isActive"
-=======
                             v-model="guild.isActive"
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                             label="Active"
                         ></v-checkbox>
                     </v-col>
                     <v-col cols="12" sm="6" md="6">
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                        <v-select
-                            v-if="user.characters.find(c => c.isActive).family.pater_familia.id === user.id"
-                            class="d-flex justify-center" variant="outlined"
-                            v-model="modifiedFamily.logo" :items="options.logos"
-                            label="Logo">
-=======
+
                         <v-select class="d-flex justify-center" variant="outlined"
                                   v-model="guild.logo" :items="options.logos"
                                   label="Logo">
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                             <template #item="{ props,item }">
                                 <v-list-item v-bind="props" title="">
                                     <v-img width="100px" :src="'/'+item.value"/>
@@ -62,20 +42,7 @@
                     </v-col>
 <!--                    <v-col cols="12" sm="6" md="6">
                         <v-text-field
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                            v-if="user.characters.find(c => c.isActive).family.pater_familia.id === user.id"
-                            v-model="modifiedFamily.animal"
-                            label="Animal"
-                        ></v-text-field>
-                        <div v-else>
-                            {{ modifiedFamily.animal }}
-                        </div>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="6">
-                        <v-select class="d-flex justify-center" variant="outlined"
-                                  v-if="modifiedFamily.pater_familia==null"
-                                  v-model="modifiedFamily.pater_familia" :items="options.characters"
-=======
+
                             v-model="guild.animal"
                             label="Animal"
                         ></v-text-field>
@@ -83,7 +50,6 @@
                     <v-col cols="12" sm="6" md="6">
                         <v-select class="d-flex justify-center" variant="outlined"
                                   v-model="guild.kingdom_id" :items="options.kingdoms"
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                                   item-title="name"
                                   item-value="id"
                                   label="Kingdom">
@@ -101,12 +67,8 @@
                     </v-col>
                     <v-col cols="12">
                         <v-textarea
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                            v-if="user.characters.find(c => c.isActive).family.pater_familia.id === user.id"
-                            v-model="modifiedFamily.description"
-=======
+
                             v-model="guild.description"
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                             label="Description"
                         ></v-textarea>
                         <div>
@@ -114,13 +76,9 @@
                         </div>
                     </v-col>
                 </v-row>
-<<<<<<< HEAD:resources/js/components/forms/FamilyForm.vue
-                <v-data-table
-                    :items="options.characters.filter(c=>c.family.id === modifiedFamily.id)"
-=======
+
 <!--                <v-data-table
                     :items="options.characters.filter(c=>c.family.id === guild.id)"
->>>>>>> ee8b7ff9dbdac153d25a934953b78d7e6fbb26e1:resources/js/components/forms/admin/GuildForm.vue
                 >
                     <template #item.rank="{ value }">
                         {{ value.name }}
